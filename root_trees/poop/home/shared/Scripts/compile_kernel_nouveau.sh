d34d42fbd99154f531379246a87668d043865f09
Sam Chambers <sam@wovac.com>
2011-08-15 07:40:55 -0500

    Removed Icon

--- root_trees/poop/home/shared/Scripts/compile_kernel_nouveau.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /usr/src/linux/
-time make && make modules_install && cp /usr/src/linux/arch/x86/boot/bzImage /boot/kernel-`date +%m-%d-%Y`-nouveau && grub-mkconfig -o /boot/grub/grub.cfg
--- root_trees/poop/home/shared/Scripts/cpuload.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#echo `uptime` | awk '{ print $4 " " $5 " " $6 " \033[1;31m"$8"\033[1;34m"$9$10}'
-echo `uptime` | awk '{ print $9$10$11}'
