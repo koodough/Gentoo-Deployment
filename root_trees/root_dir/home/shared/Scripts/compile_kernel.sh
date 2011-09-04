19475d393a9489033f3ff55aa0b0f01dd55f3515
Sam Chambers <sam@wovac.com>
2011-09-04 04:01:07 -0500

    Ask for threads when compiling kernel

--- root_trees/root_dir/home/shared/Scripts/compile_kernel.sh
+++ root_trees/root_dir/home/shared/Scripts/compile_kernel.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
+!/bin/bash
 cd /usr/src/linux/
-time make && make modules_install && cp /usr/src/linux/arch/x86/boot/bzImage /boot/kernel-`date +%m-%d-%Y` && grub-mkconfig -o /boot/grub/grub.cfg
+
+#How many threads
+if [[ -n $1 ]] ; then
+J="-j$1"
+fi
+
+time make $J && make modules_install && cp /usr/src/linux/arch/x86/boot/bzImage /boot/kernel-`date +%m-%d-%Y` && grub-mkconfig -o /boot/grub/grub.cfg
