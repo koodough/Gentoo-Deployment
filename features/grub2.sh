944e6819817efe583fbe95df1f9280e0ea84a00b
koodough <koodough@koodough.com>
2012-03-12 21:43:08 -0500

    Update grub2 to follow http://en.gentoo-wiki.com/wiki/Grub2

--- features/grub2.sh
+++ features/grub2.sh
@@ -2,7 +2,10 @@
 #GRUB2
 #http://en.gentoo-wiki.com/wiki/Grub2
 
-PACKAGES="$PACKAGES sys-boot/grub sys-boot/os-prober";
-gentoo_commander pre_install "echo -e \"#Grub2\\n<sys-boot/grub-9999 **\\nsys-boot/os-prober\" >> /etc/portage/package.keywords"
+PACKAGES="$PACKAGES sys-boot/grub:2 sys-boot/os-prober";
+gentoo_commander pre_install "echo \"sys-boot/grub:2\" >> /etc/portage/package.accept_keywords"
+gentoo_commander pre_install "echo \"sys-boot/grub:2\" >> /etc/portage/package.unmask"
 gentoo_commander post_install "grep -v rootfs /proc/mounts > /etc/mtab"
-gentoo_commander post_message "Grub: HEY! This script is definitely not installing grub for you. It's too dangerous. Package is install, now you run a command something like this in the gentoo root\n\ngrub-install --boot-directory=/$ROOT_PARTITION"
+gentoo_commander post_install "grub2-mkconfig -o /boot/grub2/grub.cfg"
+gentoo_commander post_message "Grub: HEY! This script is definitely not installing grub for you. It's too dangerous. Package is install, now you run a command something like this in the 
+gentoo root\n\ngrub-install --boot-directory=/$ROOT_PARTITION"
